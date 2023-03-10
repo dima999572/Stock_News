0d43b9a2414260efc91f1da7ced2adec689bbd2f
dima999572 <102175283+dima999572@users.noreply.github.com>
2023-03-10 12:08:22 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+import requests
+import datetime as dt
+import vonage
+
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+url_alphavantage = "https://www.alphavantage.co/query"
+params_alphavantage = {
+    "function": "TIME_SERIES_DAILY_ADJUSTED",
+    "symbol": "TSLA",
+    # "apikey": os.environ["ALPHAVANTAGE_API_KEY"]
+    "apikey": "HZV2JIPV3S3CWPA8"
+}
+
+
+response_alphavantage = requests.get(url=url_alphavantage, params=params_alphavantage)
+# Or to get yesterday and day before yesterday I can use comprehension
+yesterday = f"{(dt.datetime.today() - dt.timedelta(days=1)).strftime('%Y-%m-%d')}"
+day_before_yesterday = f"{(dt.datetime.today() - dt.timedelta(days=2)).strftime('%Y-%m-%d')}"
+yesterday_data = float(response_alphavantage.json()["Time Series (Daily)"][yesterday]["4. close"])
+day_before_yesterday_data = float(response_alphavantage.json()["Time Series (Daily)"][day_before_yesterday]["4. close"])
+average = ((yesterday_data + day_before_yesterday_data) / 2)
+percentage = round(((yesterday_data - day_before_yesterday_data) / average) * 100, 2)
+ 
+
+url_news_api = "https://newsapi.org/v2/everything?"
+params_news_api = {
+    "q": "TSLA",
+    "from": yesterday_data,
+    "sortBy": "popularity",
+    "apiKey": "387ea5073be24d7ab86a75d6e488d0fa"
+}
+
+response_news_api = requests.get(url=url_news_api, params=params_news_api)
+news = response_news_api.json()["articles"][0:3]
+
+if percentage <= -2 or percentage >= 2:
+    if percentage > 0:
+        message = [f"TSLA: grow {abs(percentage)}\nHeadline: {data['title']}\nBrief: {data['description']}" for data in
+                   news]
+    else:
+        message = [f"TSLA: drop {abs(percentage)}\nHeadline: {data['title']}\nBrief: {data['description']}" for data in
+                   news]
+else:
+    print("No any news")
+
+
+
+if message is not None:
+    end_message = ""
+
+    for data in message:
+        end_message += data + "\n\n"
+
+    client = vonage.Client(key="fdf3bf26", secret="AIsVS77WQRZ4QCs9")
+    sms = vonage.Sms(client)
+
+    print(end_message)
+
+    response_data = sms.send_message({
+        "from": "Vonage APIs",
+        "to": "48577428858",
+        "text": end_message
+    }
+    )
+
